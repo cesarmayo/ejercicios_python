6ec4661cf320ee69fa78c9accf283c19feb4d9e0
cesarmayo <cesarmayo77@gmail.com>
2020-09-21 14:21:19 -0500

    Update test.py

--- test.py
+++ test.py
@@ -13,3 +13,4 @@ El jugador que ha sacado Papel gana al jugador que ha sacado Piedra.
 Noticia relacionada: Robot que gana siempre a este juego (septiembre de 2015).
 Resuelva este ejercicio utilizando la función random.randrange(), de manera que el valor 1 corresponda a Piedra, el valor 2 corresponda a papel y el valor 3 corresponda a Tijera.
 """
+#Le estoy agregando una linea mas a este archivo
